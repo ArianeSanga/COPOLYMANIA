d085f3aa993f650226811c5dd8404632dff5cc9d
ellengoncalves <141193456+ellengoncalves@users.noreply.github.com>
2023-12-08 21:43:38 -0300

    Update jogador.c

--- COPOLYMANIA/monopoly3/Monopoly/jogador.c
+++ COPOLYMANIA/monopoly3/Monopoly/jogador.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 #include "jogador.h"
 
-
 int sortear() {
   return rand() % 6 + 1;
 }
 
 int lancaDados() {
-  
-
   int dado1 = sortear();
   int dado2 = sortear();
   int soma = dado1 + dado2;
 
-  
   imprime_Dado(dado1);
   imprime_Dado(dado2);
 
   printf("Ande %d casas!",soma);
-
-
   return soma;
 }
 
